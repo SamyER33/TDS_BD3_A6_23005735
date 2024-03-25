919689ad3c834344cda3801e10357202a2466f36
SamyER33 <125333605+SamyER33@users.noreply.github.com>
2024-03-24 19:13:40 -0600

    Segundo cambio

--- Supermercado.sql
+++ Supermercado.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `supermercado` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `supermercado`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: supermercado
@@ -25,13 +23,14 @@ DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `clientes` (
-  `ID_Cliente` int NOT NULL,
-  `Nombre` varchar(50) DEFAULT NULL,
-  `Apellido` varchar(50) DEFAULT NULL,
+  `ID_Cliente` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL,
+  `Apellido` varchar(50) NOT NULL,
   `Dirección` varchar(100) DEFAULT NULL,
   `Teléfono` varchar(15) DEFAULT NULL,
+  `Correo` varchar(255) NOT NULL,
   PRIMARY KEY (`ID_Cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,92 +39,8 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (1,'Juan','Pérez','Calle 123, Ciudad','123-456-7890'),(2,'María','Gómez','Avenida 456, Pueblo','987-654-3210');
+INSERT INTO `clientes` VALUES (1,'Juan','Pérez','Calle 123, Ciudad','123-456-7890','juan@example.com'),(2,'María','Gómez','Avenida 456, Pueblo','987-654-3210','maria@example.com');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `detalle_transacciones`
---
-
-DROP TABLE IF EXISTS `detalle_transacciones`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `detalle_transacciones` (
-  `ID_Detalle` int NOT NULL,
-  `ID_Transacción` int DEFAULT NULL,
-  `ID_Producto` int DEFAULT NULL,
-  `Cantidad` int DEFAULT NULL,
-  `Precio_Unitario` decimal(10,2) DEFAULT NULL,
-  `Subtotal` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`ID_Detalle`),
-  KEY `ID_Transacción` (`ID_Transacción`),
-  KEY `ID_Producto` (`ID_Producto`),
-  CONSTRAINT `detalle_transacciones_ibfk_1` FOREIGN KEY (`ID_Transacción`) REFERENCES `transacciones` (`ID_Transacción`),
-  CONSTRAINT `detalle_transacciones_ibfk_2` FOREIGN KEY (`ID_Producto`) REFERENCES `productos` (`ID_Producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `detalle_transacciones`
---
-
-LOCK TABLES `detalle_transacciones` WRITE;
-/*!40000 ALTER TABLE `detalle_transacciones` DISABLE KEYS */;
-INSERT INTO `detalle_transacciones` VALUES (1,1,1,5,2.50,12.50),(2,2,2,2,1.20,2.40);
-/*!40000 ALTER TABLE `detalle_transacciones` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `gananciaspordia`
---
-
-DROP TABLE IF EXISTS `gananciaspordia`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `gananciaspordia` (
-  `ID_Ganancia` int NOT NULL AUTO_INCREMENT,
-  `Fecha` date DEFAULT NULL,
-  `Ganancia` decimal(10,2) DEFAULT NULL,
-  `NuevaColumna` int DEFAULT NULL,
-  PRIMARY KEY (`ID_Ganancia`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gananciaspordia`
---
-
-LOCK TABLES `gananciaspordia` WRITE;
-/*!40000 ALTER TABLE `gananciaspordia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `gananciaspordia` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `productos`
---
-
-DROP TABLE IF EXISTS `productos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `productos` (
-  `ID_Producto` int NOT NULL,
-  `Nombre` varchar(100) DEFAULT NULL,
-  `Descripción` text,
-  `Precio` decimal(10,2) DEFAULT NULL,
-  `Stock` int DEFAULT NULL,
-  PRIMARY KEY (`ID_Producto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `productos`
---
-
-LOCK TABLES `productos` WRITE;
-/*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Leche','Leche entera, 1 litro',2.50,100),(2,'Pan','Pan blanco, paquete de 500g',1.20,200);
-/*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -136,14 +51,14 @@ DROP TABLE IF EXISTS `transacciones`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `transacciones` (
-  `ID_Transacción` int NOT NULL,
+  `ID_Transacción` int NOT NULL AUTO_INCREMENT,
   `ID_Cliente` int DEFAULT NULL,
   `Fecha` date DEFAULT NULL,
   `Total` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`ID_Transacción`),
-  KEY `ID_Cliente` (`ID_Cliente`),
-  CONSTRAINT `transacciones_ibfk_1` FOREIGN KEY (`ID_Cliente`) REFERENCES `clientes` (`ID_Cliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_cliente_transaccion` (`ID_Cliente`),
+  CONSTRAINT `fk_cliente_transaccion` FOREIGN KEY (`ID_Cliente`) REFERENCES `clientes` (`ID_Cliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,40 +84,8 @@ SET @saved_cs_client     = @@character_set_client;
  1 AS `Nombre`,
  1 AS `Apellido`,
  1 AS `Dirección`,
- 1 AS `Teléfono`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Temporary view structure for view `vista_detalle_transacciones`
---
-
-DROP TABLE IF EXISTS `vista_detalle_transacciones`;
-/*!50001 DROP VIEW IF EXISTS `vista_detalle_transacciones`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `vista_detalle_transacciones` AS SELECT 
- 1 AS `ID_Detalle`,
- 1 AS `ID_Transacción`,
- 1 AS `ID_Producto`,
- 1 AS `Cantidad`,
- 1 AS `Precio_Unitario`,
- 1 AS `Subtotal`*/;
-SET character_set_client = @saved_cs_client;
-
---
--- Temporary view structure for view `vista_productos`
---
-
-DROP TABLE IF EXISTS `vista_productos`;
-/*!50001 DROP VIEW IF EXISTS `vista_productos`*/;
-SET @saved_cs_client     = @@character_set_client;
-/*!50503 SET character_set_client = utf8mb4 */;
-/*!50001 CREATE VIEW `vista_productos` AS SELECT 
- 1 AS `ID_Producto`,
- 1 AS `Nombre`,
- 1 AS `Descripción`,
- 1 AS `Precio`,
- 1 AS `Stock`*/;
+ 1 AS `Teléfono`,
+ 1 AS `Correo`*/;
 SET character_set_client = @saved_cs_client;
 
 --
@@ -238,16 +121,18 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `ActualizarCliente`(
      IN p_Nombre VARCHAR(50),
      IN p_Apellido VARCHAR(50),
      IN p_Dirección VARCHAR(100),
-     IN p_Teléfono VARCHAR(15)
+     IN p_Teléfono VARCHAR(15),
+     IN p_Correo VARCHAR(255)
  )
 BEGIN
-     UPDATE Clientes
+     UPDATE clientes
      SET Nombre = p_Nombre,
          Apellido = p_Apellido,
          Dirección = p_Dirección,
-         Teléfono = p_Teléfono
+         Teléfono = p_Teléfono,
+         Correo = p_Correo
      WHERE ID_Cliente = p_ID_Cliente;
- END ;;
+END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -267,34 +152,9 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `EliminarCliente`(
      IN p_ID_Cliente INT
  )
 BEGIN
-     DELETE FROM Clientes
+     DELETE FROM clientes
      WHERE ID_Cliente = p_ID_Cliente;
- END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `insertarCliente` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertarCliente`(
-     IN p_nombre VARCHAR(45),
-     IN p_correo VARCHAR(225),
-     IN p_sedes INT,
-     IN p_numeroTelefono INT
- )
-BEGIN
-     INSERT INTO clientes(Nombre, correo, sedes, numeroTelefono)
-     VALUES (p_nombre, p_correo, p_sedes, p_numeroTelefono);
- END ;;
+END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
@@ -314,43 +174,7 @@ DELIMITER ;
 /*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `vista_clientes` AS select `clientes`.`ID_Cliente` AS `ID_Cliente`,`clientes`.`Nombre` AS `Nombre`,`clientes`.`Apellido` AS `Apellido`,`clientes`.`Dirección` AS `Dirección`,`clientes`.`Teléfono` AS `Teléfono` from `clientes` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `vista_detalle_transacciones`
---
-
-/*!50001 DROP VIEW IF EXISTS `vista_detalle_transacciones`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `vista_detalle_transacciones` AS select `detalle_transacciones`.`ID_Detalle` AS `ID_Detalle`,`detalle_transacciones`.`ID_Transacción` AS `ID_Transacción`,`detalle_transacciones`.`ID_Producto` AS `ID_Producto`,`detalle_transacciones`.`Cantidad` AS `Cantidad`,`detalle_transacciones`.`Precio_Unitario` AS `Precio_Unitario`,`detalle_transacciones`.`Subtotal` AS `Subtotal` from `detalle_transacciones` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-
---
--- Final view structure for view `vista_productos`
---
-
-/*!50001 DROP VIEW IF EXISTS `vista_productos`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8mb4 */;
-/*!50001 SET character_set_results     = utf8mb4 */;
-/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `vista_productos` AS select `productos`.`ID_Producto` AS `ID_Producto`,`productos`.`Nombre` AS `Nombre`,`productos`.`Descripción` AS `Descripción`,`productos`.`Precio` AS `Precio`,`productos`.`Stock` AS `Stock` from `productos` */;
+/*!50001 VIEW `vista_clientes` AS select `clientes`.`ID_Cliente` AS `ID_Cliente`,`clientes`.`Nombre` AS `Nombre`,`clientes`.`Apellido` AS `Apellido`,`clientes`.`Dirección` AS `Dirección`,`clientes`.`Teléfono` AS `Teléfono`,`clientes`.`Correo` AS `Correo` from `clientes` */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -382,4 +206,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-23 23:15:49
+-- Dump completed on 2024-03-24 19:12:40
